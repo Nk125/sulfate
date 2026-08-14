b5734a2aad562399b5252c3008cb5ef627c3afa5
Nekes <nekes@riseup.net>
2026-08-14 16:31:51 -0600

    feat: Add rkyv primitives to video feed struct

--- sulfate_core/src/video_feed.rs
+++ sulfate_core/src/video_feed.rs
@@ -1,4 +1,7 @@
-#[derive(Debug)]
+use rkyv::{Archive, Deserialize, Serialize};
+
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+#[rkyv(compare(PartialEq), derive(Debug))]
 pub struct RawVideoFrame {
     pub width: u32,
     pub height: u32,
